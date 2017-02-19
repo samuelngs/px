09ccef140e5ff2eceec6fc186318547d651baa9e
Samuel <sam@infinitely.io>
2017-02-19 16:47:11 -0700

    Update ios and android application component

--- index.ios.js
+++ index.ios.js
@@ -1,27 +1,5 @@
 
-import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
+import App from './app.js';
 
-import Navigation from './components/Navigation';
-import Controller from './components/Controller';
-
-import NavigationLogo from './components/NavigationLogo';
-import NavigationMenu from './components/NavigationMenu';
-import NavigationSearch from './components/NavigationSearch';
-
-import Home from './ui/Home';
-import Photo from './ui/Photo';
-
-export default class px extends Component {
-
-  render() {
-    return (
-      <Navigation>
-        <Controller id="home" title={<NavigationLogo />} leftButton={<NavigationMenu />} rightButton={<NavigationSearch />} component={Home} />
-        <Controller id="photo" title="Photo" component={Photo} />
-      </Navigation>
-    );
-  }
-}
-
-AppRegistry.registerComponent('px', () => px);
+AppRegistry.registerComponent('px', () => App);
