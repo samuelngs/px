85feac053ce41540f9ae15976e451e46d5f6a704
Samuel <sam@infinitely.io>
2017-02-21 21:40:13 -0700

    Simplied `ProgressiveImage` component

--- components/ProgressiveImage/index.js
+++ components/ProgressiveImage/index.js
@@ -7,23 +7,19 @@ import { LazyloadView } from 'react-native-lazyload';
 export default class ProgressiveImage extends Component {
 
   static defaultProps = {
-    host  : '',
-    width : 200,
-    height: 200,
-    radius: 0,
-    bg    : '#fff',
-    uri   : '',
-    style : { },
+    host      : '',
+    bg        : 'transparent',
+    source    : '',
+    resizeMode: 'cover',
+    style     : { },
   }
 
   static propTypes = {
-    host  : React.PropTypes.string,
-    width : React.PropTypes.number,
-    height: React.PropTypes.number,
-    radius: React.PropTypes.number,
-    bg    : React.PropTypes.string,
-    uri   : React.PropTypes.string,
-    style : React.PropTypes.object,
+    host      : React.PropTypes.string,
+    bg        : React.PropTypes.string,
+    source    : React.PropTypes.string,
+    resizeMode: React.PropTypes.string,
+    style     : React.PropTypes.object,
   }
 
   state = {
@@ -49,15 +45,15 @@ export default class ProgressiveImage extends Component {
   }
 
   render() {
-    const { host, width, height, radius: borderRadius, bg: backgroundColor, uri, style } = this.props;
+    const { host, bg, resizeMode, source, style } = this.props;
     const { opacity } = this.state;
     const Wrapper = host ? LazyloadView : View;
-    return <Wrapper host={host} ref={n => this.node = n} style={[{ width, height, borderRadius, backgroundColor }, style]}>
+    return <Wrapper host={host} ref={n => this.node = n} style={[style, { backgroundColor: bg }]}>
       <Animated.Image
-        resizeMode="cover"
-        style={[{ opacity, width, height, borderRadius }, style]}
-        source={{ uri }}
-        onLoad={ this.onLoad }
+        resizeMode={resizeMode}
+        style={[style, { opacity }]}
+        source={{uri: source}}
+        onLoad={this.onLoad}
       />
     </Wrapper>
   }
