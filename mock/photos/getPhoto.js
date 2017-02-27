efc5e46b125062c4b518b83d167e38f91c3947b7
Samuel <sam@infinitely.io>
2017-02-26 21:20:14 -0700

    Update mock photo data

--- mock/photos/getPhoto.js
+++ mock/photos/getPhoto.js
@@ -1,2 +1,72 @@
 
-export default {"id":"PzYiCWOHtfU","created_at":"2017-02-10T04:22:25-05:00","width":4392,"height":6587,"color":"#FCA971","downloads":429,"likes":82,"liked_by_user":false,"exif":{"make":"Canon","model":"Canon EOS 5D Mark III","exposure_time":"1/4000","aperture":"6.3","focal_length":"70","iso":100},"location":{"title":"B&B Expo Milano, Settimo Milanese, Italy","name":"B&B Expo Milano","city":"Settimo Milanese","country":"Italy","position":{"latitude":45.4867761,"longitude":9.05923340000004}},"current_user_collections":[],"urls":{"raw":"https://images.unsplash.com/photo-1486718448742-163732cd1544","full":"https://images.unsplash.com/photo-1486718448742-163732cd1544?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&s=2ee9f5734cc39b535d9d4969bb46f962","regular":"https://images.unsplash.com/photo-1486718448742-163732cd1544?ixlib=rb-0.3.5…crop=entropy&cs=tinysrgb&w=1080&fit=max&s=0136678ce85731bb98d95625ffa818cc","small":"https://images.unsplash.com/photo-1486718448742-163732cd1544?ixlib=rb-0.3.5…&crop=entropy&cs=tinysrgb&w=400&fit=max&s=d9fa003295ae1554a2a114df206bc51c","thumb":"https://images.unsplash.com/photo-1486718448742-163732cd1544?ixlib=rb-0.3.5…&crop=entropy&cs=tinysrgb&w=200&fit=max&s=10f5572c323508c463cbe7da1b375f03","custom":"https://images.unsplash.com/photo-1486718448742-163732cd1544?ixlib=rb-0.3.5…ntropy&cs=tinysrgb&w=&h=&rect=&fit=crop&s=6c24a9daf67d8c6d135e2dd617d7d201"},"categories":[],"links":{"self":"https://api.unsplash.com/photos/PzYiCWOHtfU","html":"http://unsplash.com/photos/PzYiCWOHtfU","download":"http://unsplash.com/photos/PzYiCWOHtfU/download","download_location":"https://api.unsplash.com/photos/PzYiCWOHtfU/download"},"user":{"id":"r01XoGoEPT8","username":"ripato","name":"Ricardo Gomez Angel","first_name":"Ricardo","last_name":"Gomez Angel","portfolio_url":"https://www.flickr.com/photos/rigoan/","bio":"","location":null,"total_likes":91,"total_photos":46,"total_collections":0,"profile_image":{"small":"https://images.unsplash.com/placeholder-avatars/extra-large.jpg?ixlib=rb-0.…op=faces&cs=tinysrgb&fit=crop&h=32&w=32&s=0ad68f44c4725d5a3fda019bab9d3edc","medium":"https://images.unsplash.com/placeholder-avatars/extra-large.jpg?ixlib=rb-0.…op=faces&cs=tinysrgb&fit=crop&h=64&w=64&s=356bd4b76a3d4eb97d63f45b818dd358","large":"https://images.unsplash.com/placeholder-avatars/extra-large.jpg?ixlib=rb-0.…=faces&cs=tinysrgb&fit=crop&h=128&w=128&s=ee8bbf5fb8d6e43aaaa238feae2fe90d"},"links":{"self":"https://api.unsplash.com/users/ripato","html":"http://unsplash.com/@ripato","photos":"https://api.unsplash.com/users/ripato/photos","likes":"https://api.unsplash.com/users/ripato/likes","portfolio":"https://api.unsplash.com/users/ripato/portfolio","following":"https://api.unsplash.com/users/ripato/following","followers":"https://api.unsplash.com/users/ripato/followers"}}}
+export default {
+  "id": "PzYiCWOHtfU",
+  "created_at": "2017-02-10T04:22:25-05:00",
+  "width": 4392,
+  "height": 6587,
+  "color": "#FCA971",
+  "downloads": 429,
+  "likes": 82,
+  "liked_by_user": false,
+  "exif": {
+    "make": "Canon",
+    "model": "Canon EOS 5D Mark III",
+    "exposure_time": "1/4000",
+    "aperture": "6.3",
+    "focal_length": "70",
+    "iso": 100
+  },
+  "location": {
+    "title": "B&B Expo Milano, Settimo Milanese, Italy",
+    "name": "B&B Expo Milano",
+    "city": "Settimo Milanese",
+    "country": "Italy",
+    "position": {
+      "latitude": 45.4867761,
+      "longitude": 9.05923340000004
+    }
+  },
+  "current_user_collections": [],
+  "urls": {
+    "raw": "https://images.unsplash.com/photo-1486718448742-163732cd1544",
+    "full": "https://images.unsplash.com/photo-1486718448742-163732cd1544?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&s=2ee9f5734cc39b535d9d4969bb46f962",
+    "regular": "https://images.unsplash.com/photo-1486718448742-163732cd1544?ixlib=rb-0.3.5…crop=entropy&cs=tinysrgb&w=1080&fit=max&s=0136678ce85731bb98d95625ffa818cc",
+    "small": "https://images.unsplash.com/photo-1486718448742-163732cd1544?ixlib=rb-0.3.5…&crop=entropy&cs=tinysrgb&w=400&fit=max&s=d9fa003295ae1554a2a114df206bc51c",
+    "thumb": "https://images.unsplash.com/photo-1486718448742-163732cd1544?ixlib=rb-0.3.5…&crop=entropy&cs=tinysrgb&w=200&fit=max&s=10f5572c323508c463cbe7da1b375f03",
+    "custom": "https://images.unsplash.com/photo-1486718448742-163732cd1544?ixlib=rb-0.3.5…ntropy&cs=tinysrgb&w=&h=&rect=&fit=crop&s=6c24a9daf67d8c6d135e2dd617d7d201"
+  },
+  "categories": [],
+  "links": {
+    "self": "https://api.unsplash.com/photos/PzYiCWOHtfU",
+    "html": "http://unsplash.com/photos/PzYiCWOHtfU",
+    "download": "http://unsplash.com/photos/PzYiCWOHtfU/download",
+    "download_location": "https://api.unsplash.com/photos/PzYiCWOHtfU/download"
+  },
+  "user": {
+    "id": "r01XoGoEPT8",
+    "username": "ripato",
+    "name": "Ricardo Gomez Angel",
+    "first_name": "Ricardo",
+    "last_name": "Gomez Angel",
+    "portfolio_url": "https://www.flickr.com/photos/rigoan/",
+    "bio": "",
+    "location": null,
+    "total_likes": 91,
+    "total_photos": 46,
+    "total_collections": 0,
+    "profile_image": {
+      "small": "https://images.unsplash.com/placeholder-avatars/extra-large.jpg?ixlib=rb-0.…op=faces&cs=tinysrgb&fit=crop&h=32&w=32&s=0ad68f44c4725d5a3fda019bab9d3edc",
+      "medium": "https://images.unsplash.com/placeholder-avatars/extra-large.jpg?ixlib=rb-0.…op=faces&cs=tinysrgb&fit=crop&h=64&w=64&s=356bd4b76a3d4eb97d63f45b818dd358",
+      "large": "https://images.unsplash.com/placeholder-avatars/extra-large.jpg?ixlib=rb-0.…=faces&cs=tinysrgb&fit=crop&h=128&w=128&s=ee8bbf5fb8d6e43aaaa238feae2fe90d"
+    },
+    "links": {
+      "self": "https://api.unsplash.com/users/ripato",
+      "html": "http://unsplash.com/@ripato",
+      "photos": "https://api.unsplash.com/users/ripato/photos",
+      "likes": "https://api.unsplash.com/users/ripato/likes",
+      "portfolio": "https://api.unsplash.com/users/ripato/portfolio",
+      "following": "https://api.unsplash.com/users/ripato/following",
+      "followers": "https://api.unsplash.com/users/ripato/followers"
+    }
+  }
+}
